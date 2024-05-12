8a318167e3df0efff3451bda984e97ba567a2ae6
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-05-12 13:52:08 -0400

    Added index files for components

--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -7,10 +7,10 @@ import Button from "./Button";
 export default function Navbar() {
   return (
     <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+      <div className="w-full h-20 bg-emerald-800 sticky top-0 navbar">
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
-            <Logo />
+            {/* <Logo /> */}
             <ul className="hidden md:flex gap-x-6 text-white">
               <li>
                 <Link href="/about">
