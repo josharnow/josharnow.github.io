dae9e3c713526a159694271601d2a1174ba7df5e
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-05-12 14:04:38 -0400

    Added variables.scss and started implementation

--- src/components/navigation/navbar/index.tsx
+++ src/components/navigation/navbar/index.tsx
@@ -3,11 +3,14 @@ import React from "react";
 import Link from "next/link";
 import Logo from "./Logo";
 import Button from "./Button";
+import variables from '@/src/styles/variables.module.scss';
 
 export default function Navbar() {
   return (
     <>
-      <div className="w-full h-20 bg-emerald-800 sticky top-0 navbar">
+      <div className="w-full h-20 sticky top-0 navbar" style={{backgroundColor: variables.primaryColor}}>
+      {/* NOTE - I'm just testing SCSS styling above */}
+      {/* <div className="w-full h-20 bg-emerald-800 sticky top-0 navbar"> */}
         <div className="container mx-auto px-4 h-full">
           <div className="flex justify-between items-center h-full">
             {/* <Logo /> */}
