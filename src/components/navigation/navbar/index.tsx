f98fe2b6723b2a93ca2d144514d80975e505af9a
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-05-12 13:20:53 -0400

    Added basic navbar component

---
+++ src/components/navigation/navbar/index.tsx
@@ -0,0 +1,37 @@
+// NOTE - https://www.reddit.com/r/reactjs/comments/17i3a1p/what_is_the_benefit_of_the_indextsx_naming/
+import React from "react";
+import Link from "next/link";
+import Logo from "./Logo";
+import Button from "./Button";
+
+export default function Navbar() {
+  return (
+    <>
+      <div className="w-full h-20 bg-emerald-800 sticky top-0">
+        <div className="container mx-auto px-4 h-full">
+          <div className="flex justify-between items-center h-full">
+            <Logo />
+            <ul className="hidden md:flex gap-x-6 text-white">
+              <li>
+                <Link href="/about">
+                  <p>About Us</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/services">
+                  <p>Services</p>
+                </Link>
+              </li>
+              <li>
+                <Link href="/contacts">
+                  <p>Contacts</p>
+                </Link>
+              </li>
+            </ul>
+            <Button />
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
