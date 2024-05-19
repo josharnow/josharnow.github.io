56aa84d2cb0676ee7113d169055244638a99a0f8
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-05-18 20:34:19 -0400

    Added firebase.ts

---
+++ src/config/firebase.ts
@@ -0,0 +1,42 @@
+// Import the functions you need from the SDKs you need
+import { getApps, initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// import { getFirestore } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const API_KEY = process.env.NEXT_PUBLIC_FIREBASE_API_KEY;
+export const AUTH_DOMAIN = process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN;
+export const PROJECT_ID = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
+export const STORAGE_BUCKET = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
+export const MESSAGING_SENDER_ID = process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID;
+export const APP_ID = process.env.NEXT_PUBLIC_FIREBASE_APP_ID;
+export const MEASUREMENT_ID = process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID;
+
+if (!API_KEY || !AUTH_DOMAIN || !PROJECT_ID || !STORAGE_BUCKET || !MESSAGING_SENDER_ID || !APP_ID || !MEASUREMENT_ID) {
+  throw new Error('Missing firebase config values');
+}
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
+  measurementId: MEASUREMENT_ID,
+};
+
+
+// Initialize Firebase
+export const firebase_app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
+// export const db = getFirestore(firebase_app);
+
+//  Extract instances of Firestore and Analytics from the initialized Firebase app
+export const analytics = getAnalytics(firebase_app);
+// export const db = getFirestore(firebase_app);
