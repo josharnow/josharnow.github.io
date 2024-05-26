dc3ea7fadb2516202ed47bf74b91ad9d619b0ec5
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-05-25 23:40:08 -0400

    Fix 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -89,7 +89,7 @@ export default async function RootLayout({
           {/* TODO - Make Sidebar component similar to the way the old navigation component was (i.e. with a toggle) */}
           {/* TODO - When the screen is "mobile sized" (i.e. small enough), make navbar operate on top similarly to old navbar. Will probably involve conditionally setting flex/flex-column on parent element here based on screen size (i.e. via Tailwind) */}
           <Navigation />
-          <main className={classNames(styles.main)}>
+          <main className="grow">
             {children}
           </main>
         </div>
