e52d466a83dac99e1cb7af04405d9d06e530ecab
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-05-27 14:26:52 -0400

    Came up with a hacky solution to correctly making the sidebar sticky

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,9 @@
 // This is the root view of the app, hence it is placed in the root of the app directory.
 import Image from "next/image";
 import Link from "next/link";
+
+import { Card } from 'primereact/card';
+
 import { classNames } from "@/src/utils";
 import styles from "./styles.module.scss";
 
@@ -11,6 +14,12 @@ export default function Home() {
         <div className="mx-auto max-w-2xl text-center flex flex-col items-center">
           <h1>Welcome to my unfinished website! I&apos;m in the process of building it.</h1><br></br>
           <Image src="https://cdn-icons-png.flaticon.com/512/8999/8999474.png" alt="Work In Progress" width={ 200 } height={ 200 } />
+          {/* TODO - iterate through cards to test scrolling */}
+
+          {Array.from({ length: 5 }).map((_, index) => (
+            <Card key={index} title={`Card ${index + 1}`} style={{ width: '25rem', height: '25rem' }} className="bg-white" />
+          ))}
+
         </div>
       </div>
     </>
--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useState } from "react";
 import Sidebar from "./Sidebar";
+import ShadowSidebar from "./ShadowSidebar";
 import Navbar from "./Navbar";
-import BackgroundMenu from "./BackgroundMenu";
+// import BackgroundMenu from "./BackgroundMenu";
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -16,6 +17,9 @@ const Navigation = () => {
 
       {/* TODO - Add button to collapse sidebar */}
       {/* TODO - Figure out how to collapse the sidebar */}
+
+      {/* NOTE - This is a hacky fix I came up with to make the sidebar sticky and still use flex-grow to grow the main content on the screen (i.e. to get the correct sizing) */}
+      <ShadowSidebar isOpen={ isOpen } toggle={ toggle } />
       <Sidebar isOpen={ isOpen } toggle={ toggle } />
       {/* <Navbar toggle={ toggle } /> */}
     </>
