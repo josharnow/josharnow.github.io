7c8d273d47040015bbf96921cdfa0929f3967eba
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-05-27 15:12:37 -0400

    Moved sidebarOption out to Navigation component

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -2,13 +2,48 @@
 import { useState } from "react";
 import Sidebar from "./Sidebar";
 import Navbar from "./Navbar";
-// import BackgroundMenu from "./BackgroundMenu";
+// import { UserIcon, Bars3Icon } from "@heroicons/react/24/solid"
+import { EnvelopeIcon, CubeTransparentIcon, IdentificationIcon } from "@heroicons/react/20/solid"; 
 
 const Navigation = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => {
     setIsOpen(!isOpen);
   };
+
+  const sidebarOptions: SidebarOption[] = [
+    // {
+    //   name: "About",
+    //   href: "/about",
+    //   icon: IdentificationIcon,
+    //   current: `/${segment}` === "/about" ? true : false,
+    // },
+    // {
+    //   name: "Contact",
+    //   href: "/contact",
+    //   icon: EnvelopeIcon,
+    //   current: `/${segment}` === "/contact" ? true : false,
+    // },
+    // {
+    //   name: "Portfolio",
+    //   href: "/portfolio",
+    //   icon: CubeTransparentIcon,
+    //   current: `/${segment}` === "/portfolio" ? true : false,
+    // },
+    {
+      name: "TEST ROUTE",
+      href: "/test_route",
+      icon: CubeTransparentIcon,
+      // current: `/${segment}` === "/test_route" ? true : false,
+    },
+    {
+      name: "DYNAMIC_TEST ROUTE",
+      href: "/test_route_dynamic/4",
+      icon: CubeTransparentIcon,
+      // current: `/${segment}`.includes('/test_route_dynamic') ? true : false,
+    },
+  ];
+
   return (
     <>
       {/* TODO - Make Sidebar (on left side) the default for desktop view & make left sidebar appear w/ menu button on mobile */ }
@@ -19,7 +54,7 @@ const Navigation = () => {
 
       {/* TODO - Maybe make a "shadowsidebar..." This sits behind the real sidebar and is used to make the main part of the screen be correctly sized */}
       {/* NOTE - This is a hacky fix I came up with to make the sidebar sticky and still use flex-grow to grow the main content on the screen (i.e. to get it to the correct size) */}
-      <Sidebar isOpen={ isOpen } toggle={ toggle } />
+      <Sidebar isOpen={ isOpen } toggle={ toggle } sidebarOptions={ sidebarOptions  } />
       {/* <Navbar toggle={ toggle } /> */}
     </>
   );
