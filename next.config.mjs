e50c693ca838c00535d63ffe576153bbca11c64b
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-05-28 21:00:22 -0400

    Upgraded to Next.js 15 & React 19

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,23 @@
+import MillionLint from '@million/lint';
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   // output: "export",
   // webpack: {},
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'avatars.githubusercontent.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'cdn-icons-png.flaticon.com',
-      },
-    ],
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'avatars.githubusercontent.com'
+    }, {
+      protocol: 'https',
+      hostname: 'cdn-icons-png.flaticon.com'
+    }]
     // unoptimized: true,
-  },
+  }
   // sassOptions: {
   //   includePaths: [path.join(__dirname, 'src/styles')],
   // }
-  
 };
-
-export default nextConfig;
+export default MillionLint.next({
+  rsc: true
+})(nextConfig);
