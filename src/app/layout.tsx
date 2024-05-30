042b6e2d31b2b1b4b1d1e663337077e2af494eb9
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-05-30 10:28:16 -0400

    Shorten URL

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,9 +18,9 @@ export const metadata: Metadata = {
   applicationName: "Josh Arnow's Website", // NOTE - This should be applied on all pages
   title: "Josh Arnow's Website", // NOTE - This should change depending on what page is being viewed. Same goes for other titles in this metadata object.
   description: "Personal website of Josh Arnow, a full stack software engineer based in New York & Florida.",
-  authors: [{name: "Josh Arnow", url: "https://www.josharnow.com"}],
+  authors: [{name: "Josh Arnow", url: "https://josharnow.com"}],
   creator: "Josh Arnow",
-  metadataBase: new URL('https://www.josharnow.com'),
+  metadataBase: new URL('https://josharnow.com'),
   generator: "Next.js",
   keywords: ["Josh Arnow", "software engineer", "full stack", "full-stack", "New York", "Florida", "software"], // TODO - Add more relevant keywords. Although it appears that Google doesn't actually look for them (https://developers.google.com/search/docs/crawling-indexing/special-tags)
   icons: {
@@ -29,7 +29,7 @@ export const metadata: Metadata = {
   },
   openGraph: { // NOTE - https://ahrefs.com/blog/open-graph-meta-tags/
     type: "website",
-    url: "https://www.josharnow.com",
+    url: "https://josharnow.com",
     siteName: "Josh Arnow's Website",
     title: "Josh Arnow's Website",
     description: "Personal website of Josh Arnow, a full stack software engineer based in New York & Florida.",
@@ -45,7 +45,7 @@ export const metadata: Metadata = {
   },
   twitter: {
     card: "summary_large_image",
-    site: "https://www.josharnow.com",
+    site: "https://josharnow.com",
     creator: "Josh Arnow",
     images: "/open-graph-image.png", // TODO - Make this an image of whatever page is being shared
     description: "Personal website of Josh Arnow, a full stack software engineer based in New York & Florida.",
