5e105ec4560abc692ecfcd661bbd3cd0edc0ccb1
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-05-30 21:08:35 -0400

    Started implementing Three.js

--- src/app/3D_Demo/page.tsx
+++ src/app/3D_Demo/page.tsx
@@ -1,13 +1,24 @@
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { Loader } from '@/src/components';
 
 export default async function ThreeDDemo() {
 
   return (
     <>
-      {/* <h1>Hello world!</h1>
-      <h1>{data.id}</h1>
-      <Image src={data.avatar_url} alt="GitHub Avatar" width={200} height={200} />
-      <Link href="/">Go Home</Link> */}
       <h1>3D demo test</h1>
+      <Canvas 
+        className='w-full h-full bg-transparent'
+        camera={{near: 0.1, far: 1000}}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+        </Suspense>
+      </Canvas>
     </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,3 @@
 export { default as Device } from './Device';
 export { default as Navigation } from './Navigation';
+export { default as Loader } from './Loader';
