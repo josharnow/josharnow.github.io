caf2524c7900a6928235c869b50e7a909abaeb5c
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-06-02 18:26:30 -0400

    More type updates

--- src/types/three.d.ts
+++ src/types/three.d.ts
@@ -1,3 +1,11 @@
 // These should make typing MeshRefs across the project easier
 type InitialMeshRef = React.MutableRefObject<import("three").Mesh | undefined>;
 type AssignedMeshRef = React.MutableRefObject<import("three").Mesh>;
+type InitialGroupRef = React.MutableRefObject<import("three").Group | undefined>;
+type AssignedGroupRef = React.MutableRefObject<import("three")>;
+// NOTE - https://github.com/pmndrs/drei/issues/469#issuecomment-897539047
+type DreiGLTF = import("three-stdlib").GLTF & {
+  // NOTE - Making this a "Record" so that it can be used as a dictionary without a TypeScript issue (e.g. using "geometry" in nodes.Object_7.geometry)
+  nodes: Record<string, import("three").SkinnedMesh>;
+  materials: Record<string, import("three").Material>;
+};
