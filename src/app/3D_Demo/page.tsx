85af2814dcd95a22c10bef9f535ead1a1f9ab472
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-06-02 15:31:32 -0400

    Made Loader component work

--- src/app/3D_Demo/page.tsx
+++ src/app/3D_Demo/page.tsx
@@ -1,8 +1,9 @@
 "use client";
+// NOTE - "use client" appears to be necessary to compile this without an error
 import { Suspense } from 'react';
 import { Canvas } from '@react-three/fiber';
-import { Loader as dreiLoader } from "@react-three/drei";
-// import { Loader } from '@/src/components';
+// import { Loader as dreiLoader } from "@react-three/drei";
+import { Loader } from '@/src/components';
 
 
 export default function ThreeDDemo() {
@@ -14,8 +15,7 @@ export default function ThreeDDemo() {
         className='w-full h-full bg-transparent'
         camera={{near: 0.1, far: 1000}}
       >
-        {/* <Suspense fallback={<Loader />}> */}
-        <Suspense>
+        <Suspense fallback={<Loader />}>
           <directionalLight />
           <ambientLight />
           <pointLight />
--- src/components/Loader/index.tsx
+++ src/components/Loader/index.tsx
@@ -1,4 +1,4 @@
-// "use client";
+"use client";
 import { Html } from "@react-three/drei";
 import React from "react";
 
@@ -6,11 +6,11 @@ const Loader = () => {
   return (
     <>
       {/* NOTE - HTML appears broken for some reason... doesn't seem to be related to Million.js */}
-      {/* <Html>
+      <Html>
         <div className='flex justify-center items-center'>
           <div className='w-20 h-20 border-2 border-opacity-20 border-blue-500 border-t-blue-500 rounded-full animate-spin'></div>
         </div>
-      </Html> */}
+      </Html>
     </>
   );
 };
