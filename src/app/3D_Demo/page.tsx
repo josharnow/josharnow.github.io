d6b01756ccc8369d8eafae611eaf0155d84e18d3
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-06-14 20:06:06 -0400

    Tested build process w/ 3D & confirmed working

--- src/app/3D_Demo/page.tsx
+++ src/app/3D_Demo/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 // NOTE - "use client" appears to be necessary to compile this without an error
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Canvas } from '@react-three/fiber';
 // import { Loader as dreiLoader } from "@react-three/drei";
 import { Loader } from '@/src/components';
+import { Island } from '@/src/models';
 
 
 export default function ThreeDDemo() {
+  const [isRotating, setIsRotating] = useState(true);
+  const [currentStage, setCurrentStage] = useState(0);
+  const currentFocusPoint = 0;
 
   return (
     <>
@@ -21,6 +25,13 @@ export default function ThreeDDemo() {
           <pointLight />
           <spotLight />
           <hemisphereLight />
+          {/* TODO - Set up props for island */}
+          <Island
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={ setCurrentStage as (stage: number | null) => void }
+            currentFocusPoint={currentFocusPoint}
+          />
         </Suspense>
       </Canvas>
     </>
