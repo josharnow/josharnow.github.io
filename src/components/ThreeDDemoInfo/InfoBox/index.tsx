af3a8a9e4a1c7a6ac1c3c92f8ba158f0e0227ea2
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-06-21 21:35:03 -0400

    Removed button text on ThreeDDemoInfo

--- src/components/ThreeDDemoInfo/InfoBox/index.tsx
+++ src/components/ThreeDDemoInfo/InfoBox/index.tsx
@@ -3,17 +3,20 @@ import "../styles.css";
 
 const InfoBox = ({ text, link, btnText }: {
   text: string;
-  link: string;
-  btnText: string;
+  link?: string;
+  btnText?: string;
 }) => (
   <div className="info-box">
     <p className='font-medium text-center sm:text-xl'>
       { text }
     </p>
-    <Link href={ link } className='neo-brutalism-white neo-btn'>
-      { btnText }
-      {/* <img src={ arrow } alt='arrow' className='w-4 h-4 object-contain' /> */ }
-    </Link>
+    {
+      link &&
+      <Link href={ link } className='neo-brutalism-white neo-btn'>
+        { btnText }
+        {/* <img src={ arrow } alt='arrow' className='w-4 h-4 object-contain' /> */ }
+      </Link>
+    }
   </div>
 );
 
--- src/components/ThreeDDemoInfo/index.tsx
+++ src/components/ThreeDDemoInfo/index.tsx
@@ -1,9 +1,5 @@
-// import { Link } from "react-router-dom";
-import Link from "next/link";
 import "./styles.css";
 import InfoBox from "./InfoBox";
-// import styles from "./styles.module.scss";
-// import { arrow } from "../assets/icons";
 
 function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
@@ -22,22 +18,22 @@ const renderContent: RenderContentObj = {
   2: (
     <InfoBox
       text="Worked with many companies and picked up many skills along the way"
-      link="/about"
-      btnText="Learn more"
+      // link="/about"
+      // btnText="Learn more"
     />
   ),
   3: (
     <InfoBox
       text="Led multiple projects to success over the years. Curious about the impact?"
-      link="/projects"
-      btnText="Visit my portfolio"
+      // link="/projects"
+      // btnText="Visit my portfolio"
     />
   ),
   4: (
     <InfoBox
       text="Need a project done or looking for a dev? I'm just a few keystrokes away"
-      link="/contact"
-      btnText="Let's talk"
+      // link="/contact"
+      // btnText="Let's talk"
     />
   ),
 }
