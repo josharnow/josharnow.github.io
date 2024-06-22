651602696812f765c7a5d6dc088874b4876c907e
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-06-21 21:22:39 -0400

    Componentized InfoBox

--- src/components/ThreeDDemoInfo/index.tsx
+++ src/components/ThreeDDemoInfo/index.tsx
@@ -1,6 +1,7 @@
 // import { Link } from "react-router-dom";
 import Link from "next/link";
 import "./styles.css";
+import InfoBox from "./InfoBox";
 // import styles from "./styles.module.scss";
 // import { arrow } from "../assets/icons";
 
@@ -19,52 +20,27 @@ const renderContent: RenderContentObj = {
     </h1>
   ),
   2: (
-    <div className="info-box">
-      <p className='font-medium sm:text-xl text-center'>
-        Worked with many companies <br /> and picked up many skills along the way
-      </p>
-
-      <Link href='/about' className='neo-brutalism-white neo-btn'>
-        Learn more
-        {/* <img src={ arrow } alt='arrow' className='w-4 h-4 object-contain' /> */ }
-      </Link>
-    </div>
+    <InfoBox
+      text="Worked with many companies and picked up many skills along the way"
+      link="/about"
+      btnText="Learn more"
+    />
   ),
   3: (
-    <div className='info-box'>
-      <p className='font-medium text-center sm:text-xl'>
-        Led multiple projects to success over the years. <br /> Curious about the impact?
-      </p>
-
-      <Link href='/projects' className='neo-brutalism-white neo-btn'>
-        Visit my portfolio
-        {/* <img src={ arrow } alt='arrow' className='w-4 h-4 object-contain' /> */ }
-      </Link>
-    </div>
+    <InfoBox
+      text="Led multiple projects to success over the years. Curious about the impact?"
+      link="/projects"
+      btnText="Visit my portfolio"
+    />
   ),
   4: (
-    <div className='info-box'>
-      <p className='font-medium sm:text-xl text-center'>
-        Need a project done or looking for a dev? <br /> I'm just a few keystrokes away
-      </p>
-
-      <Link href='/contact' className='neo-brutalism-white neo-btn'>
-        Let's talk
-        {/* <img src={ arrow } alt='arrow' className='w-4 h-4 object-contain' /> */ }
-      </Link>
-    </div>
+    <InfoBox
+      text="Need a project done or looking for a dev? I'm just a few keystrokes away"
+      link="/contact"
+      btnText="Let's talk"
+    />
   ),
 }
-
-const InfoBox = ({ text, link, btnText }: {
-  text: string;
-  link: string;
-  btnText: string;
-}) => (
-  <div className="info-box">
-    {text}
-  </div>
-)
 
 const ThreeDDemoInfo = ({ currentStage }: {
   currentStage: number;
