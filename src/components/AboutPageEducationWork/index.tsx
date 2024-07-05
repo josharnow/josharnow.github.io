9f5bcd159c0f7132a3f23b5ac326b5404541f6ee
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-07-05 15:27:22 -0400

    Extensive modification of GoogleGeminiEffect to fit my needs

--- src/components/AboutPageEducationWork/index.tsx
+++ src/components/AboutPageEducationWork/index.tsx
@@ -17,7 +17,9 @@ const AboutPageEducationWork = () => {
   const pathLengthFifth = useTransform(scrollYProgress, [0, 0.4], [0, 1.2]);
 
   return (
-    <div ref={ ref } className="h-full">
+    // <div ref={ ref } className="h-full" style={ { "backgroundColor": "rgb(24 24 27)" } }>
+    <div ref={ ref } className="h-full" style={ { "backgroundImage": "linear-gradient(rgb(24 24 27), black)" } }>
+      {/* <div>TEST QUOPTE</div> */}
       <GoogleGeminiEffect
         pathLengths={ [
           pathLengthFirst,
@@ -26,6 +28,8 @@ const AboutPageEducationWork = () => {
           pathLengthFourth,
           pathLengthFifth,
         ] }
+        // description="We are more than the sum total of our experiences, but a diverse background has equipped me with a unique perspective"
+        description="We are more than the sum total of our experiences, but my diverse background equips me with a unique perspective to offer"
       />
     </div>
   );
