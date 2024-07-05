d972dd79c067ec0a95aed2d4fda12690ec97ba29
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-07-05 16:22:05 -0400

    Began adding quote

--- src/components/AboutPageEducationWork/index.tsx
+++ src/components/AboutPageEducationWork/index.tsx
@@ -17,9 +17,7 @@ const AboutPageEducationWork = () => {
   const pathLengthFifth = useTransform(scrollYProgress, [0, 0.4], [0, 1.2]);
 
   return (
-    // <div ref={ ref } className="h-full" style={ { "backgroundColor": "rgb(24 24 27)" } }>
     <div ref={ ref } className="h-full" style={ { "backgroundImage": "linear-gradient(rgb(24 24 27), black)" } }>
-      {/* <div>TEST QUOPTE</div> */}
       <GoogleGeminiEffect
         pathLengths={ [
           pathLengthFirst,
@@ -28,14 +26,16 @@ const AboutPageEducationWork = () => {
           pathLengthFourth,
           pathLengthFifth,
         ] }
-        // description="We are more than the sum total of our experiences, but a diverse background has equipped me with a unique perspective"
         description="We are more than the sum total of our experiences, but my diverse background equips me with a unique perspective to offer"
       >
-        <div>
-          test
+        {/* <div className="px-2 md:px-40"> */}
+        <div className="px-20 md:px-40">
+          <q cite="https://www.goodreads.com/quotes/482424-true-happiness-comes-from-the-joy-of-deeds-well-done">
+            <i>True happiness comes from the joy of deeds well done, the zest of creating things new.</i>
+          </q> ~Antoine de Saint-Exupéry
+          {/* TODO - bring ~Antoine de Saint-Exupéry to the next line and align right */}
         </div>
       </GoogleGeminiEffect>
-
     </div>
   );
 };
