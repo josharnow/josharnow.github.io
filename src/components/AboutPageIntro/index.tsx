0df2168dca555e8179192f1bf70a57b772ae1e71
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-07-04 22:56:08 -0400

    Mostly working sizing

---
+++ src/components/AboutPageIntro/index.tsx
@@ -0,0 +1,46 @@
+"use client";
+import React from "react";
+import { motion } from "framer-motion";
+import { AuroraBackground } from "@/src/components";
+
+const AboutPageIntro = () => {
+  return (
+    // <AuroraBackground className="min-h-fit h-full w-full">
+    <AuroraBackground className="min-h-fit h-full w-full">
+      <motion.div
+        initial={ { opacity: 0.0, y: 0 } }
+        whileInView={ { opacity: 1.0, y: 0 } }
+        transition={ {
+          delay: 0.3,
+          duration: 0.8,
+          ease: "easeInOut",
+        } }
+        className="h-fit px-6 py-0 sm:py-32 sm:px-24"
+        // className="min-h-full px-6 py-24 sm:py-32 sm:px-24"
+      >
+        <div className="h-full min-h-fit relative flex flex-col justify-between">
+          <div className="flex flex-col grow justify-between w-3/6">
+            <h1 className="text-white text-balance text-3xl md:text-4xl lg:text-5xl">
+              Hi, I’m <b className="font-medium text-blue-500">Josh</b>
+            </h1>
+          </div>
+          <div className="w-full flex grow justify-end">
+            <div className="h-full flex w-3/6 text-right justify-end" style={ { "textAlign": "justify" } }>
+              <h1 className="text-white self-center text-right text-balance text-3xl md:text-4xl lg:text-5xl">
+                A <b className="text-blue-500 font-medium">software engineer</b> from NY/FL
+              </h1>
+            </div>
+          </div>
+          <div className="flex flex-col grow justify-end text-white w-3/6 text-3xl lg:text-4xl">
+            {/* <div className="text-white w-3/6 text-3xl sm:text-4xl"> */ }
+            {/* TODO - Typewriter Aceternity effect */ }
+            <p className="">
+              Software is a more than a career to me:<br />it’s a <b className="text-red-500 font-medium">passion</b>
+            </p>
+          </div>
+        </div>
+      </motion.div>
+    </AuroraBackground>
+  );
+};
+export default AboutPageIntro;
