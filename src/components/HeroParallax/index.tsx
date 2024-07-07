3bc01038d6d6977523b90d8b4163abd68f782150
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-07-07 17:01:11 -0400

    Made ProductCard fit screenshots

--- src/components/HeroParallax/index.tsx
+++ src/components/HeroParallax/index.tsx
@@ -214,7 +214,7 @@ export const ProductCard = ({
         y: -20,
       } }
       key={ product.title }
-      className="group/product h-96 w-[30rem] relative flex-shrink-0"
+      className="group/product h-96 w-[40rem] relative flex-shrink-0"
     >
       <Link
         href={ product.link }
