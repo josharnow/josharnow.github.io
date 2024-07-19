4c3164849429d18a71c4c2d7a5865e18de846e84
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-07-19 12:37:04 -0400

    Completed Navbar

--- src/components/Navigation/Navbar/index.tsx
+++ src/components/Navigation/Navbar/index.tsx
@@ -1,7 +1,5 @@
 // NOTE - https://www.reddit.com/r/reactjs/comments/17i3a1p/what_is_the_benefit_of_the_indextsx_naming/
 import React from "react";
-// import Logo from "./Logo";
-// import variables from '@/src/styles/variables.module.scss';
 import styles from './styles.module.scss';
 import { ContactModal } from "@/src/components";
 import NavbarMenu from "./NavbarMenu";
@@ -25,18 +23,17 @@ const Navbar = ({ toggle, isOpen, navigationOptions }: {
           ) }
         >
           <div className="flex justify-between items-center h-full w-full px-4 py-2">
-            {/* <div className="p-3" style={{ height: '100%', aspectRatio: '1/1' }}>
-              <Logo />
-            </div> */}
-            <ContactModal />
-            <button className="px-4 py-2 rounded-md text-white text-center relative overflow-hidden bg-black flex justify-center shadow-3xl shadow-blue-500 w-fit group/resume-btn" onClick={ () => console.log("CLICKED") }>
-              <span className="group-hover/resume-btn:translate-x-40 text-center transition duration-500">
-                📄 Résumé
-              </span>
-              <div className="-translate-x-40 group-hover/resume-btn:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500 text-black z-20">
-                📈
-              </div>
-            </button>
+            <div className="flex gap-x-4">
+              <ContactModal />
+              <button className="px-4 py-2 rounded-md text-white text-center relative overflow-hidden bg-black flex justify-center shadow-3xl shadow-blue-500 w-fit group/resume-btn" onClick={ () => console.log("CLICKED") }>
+                <span className="group-hover/resume-btn:translate-x-40 text-center transition duration-500">
+                  📄 Résumé
+                </span>
+                <div className="-translate-x-40 group-hover/resume-btn:translate-x-0 flex items-center justify-center absolute inset-0 transition duration-500 text-black z-20">
+                  📈
+                </div>
+              </button>
+            </div>
             <NavbarMenuButton toggle={ toggle } isOpen={ isOpen } />
           </div>
         </div>
