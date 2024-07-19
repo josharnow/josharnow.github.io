a0f80f53994ad457a3b2f638ad38c473bdaa6aa8
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-07-19 12:45:21 -0400

    Made contact button more modular

--- src/components/AboutPageContact/index.tsx
+++ src/components/AboutPageContact/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React, { forwardRef } from "react";
-import { MovingBorderButton } from "@/src/components";
+import { MovingBorderButton, ContactModal } from "@/src/components";
 import { motion } from "framer-motion";
 import Link from "next/link";
 
