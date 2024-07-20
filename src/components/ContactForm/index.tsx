2839f83274af2eee25c4b8210ec4015d50ccb957
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-07-20 13:32:50 -0400

    Improved contact flow

--- src/components/ContactForm/index.tsx
+++ src/components/ContactForm/index.tsx
@@ -22,14 +22,22 @@ function cn(...inputs: ClassValue[]) {
 
 // SOURCE - https://dsavir-h.medium.com/contact-form-for-static-site-with-web3forms-575ee166732
 function ContactForm({ formId }: { formId?: string } ) {
+  const nameElement = useRef<HTMLInputElement>(null);
+  const emailElement = useRef<HTMLInputElement>(null);
+  const message = useRef<HTMLSpanElement>(null);
+
   const [captcha, setCaptcha] = useState("");
+
 
   async function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
+    
     if (!captcha) {
       alert("Please verify the captcha");
       return;
     }
+    const messageElement = document.querySelector('#message');
+    const messageContent = messageElement?.textContent;
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
@@ -40,16 +48,16 @@ function ContactForm({ formId }: { formId?: string } ) {
       body: JSON.stringify({
         access_key: process.env.NEXT_PUBLIC_WEB3FORMS_API_KEY,
         "h-captcha-response": captcha,
-        name: (e.target).name.value,
-        email: (e.target).email.value,
-        message: (e.target).message.value,
+        name: nameElement.current?.value,
+        email: emailElement.current?.value,
+        // TODO - Switch out w/ state version
+        message: messageContent,
       }),
     });
     const result = await response.json();
     // if (response.ok) {
 
     // }
-    // TODO - Expand message box down (flex grow...?)
     // TODO - After submission, show a success message under the form
     // TODO - Do a similar message with the captcha instead of presenting an alert
     // TODO - Use state to retain information a user puts in the boxes in case they accidentally click out. State handler will probably have to be in parent component, with the set function passed in here
@@ -60,26 +68,22 @@ function ContactForm({ formId }: { formId?: string } ) {
       <form id={ formId } onSubmit={ handleSubmit } className="flex flex-col gap-y-2">
         <FormLabelInputContainer className="w-full sm:w-[50%]">
             <FormLabel htmlFor="name">Name</FormLabel>
-            <FormInput id="name" name="name" placeholder="Your name" type="name" className="" />
+          <FormInput ref={ nameElement } id="name" name="name" placeholder="Your name" type="name" className="" />
         </FormLabelInputContainer>
         <FormLabelInputContainer className="w-full sm:w-[75%]">
           <FormLabel htmlFor="email">Email</FormLabel>
-          <FormInput id="email" placeholder="YourEmailAddress@example.com" type="email" name="email" className="" />
+          <FormInput ref={ emailElement } id="email" placeholder="YourEmailAddress@example.com" type="email" name="email" className="" />
         </FormLabelInputContainer>
         <FormLabelInputContainer className="w-full">
           <FormLabel htmlFor="message">Message</FormLabel>
           <FormInput id="message" placeholder="Your message" name="message" className="h-20" inputElement={(
             <>
             {/* NOTE - https://css-tricks.com/auto-growing-inputs-textareas/ */}
-              <span role="textbox" contentEditable={ true } id="message" 
-                className={ cn(styles.textareaInput, "border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent  file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600  focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400")} />
-              {/* <textarea id="message" name="message" placeholder="Your message"
-                  className="grow flex h-full w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
-                  file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
-                  focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
-                  disabled:cursor-not-allowed disabled:opacity-50
-                  dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
-                  group-hover/input:shadow-none transition duration-400" /> */}
+            {/* // <span onChange={(e) => setMessageContent((e.target))} role="textbox" contentEditable={ true } id="message"  */}
+              <span ref={message} role="textbox" contentEditable={ true } id="message" 
+                className={ cn(styles.textareaInput, "border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent  file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600  focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400")} 
+                
+                />
             </>
           )} />
         </FormLabelInputContainer>
