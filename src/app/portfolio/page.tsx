a4d8b1a4df31304fac4fde86463da6b6478539ac
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-07-21 13:16:24 -0400

    Added "coming soon" pages

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React from 'react';
+import styles from './styles.module.scss';
+
+export default function Portfolio() {
+
+  return (
+    <>
+      <div className='h-full flex justify-center items-center'>
+        <span className='font-medium text-3xl'>Coming soon!</span>
+      </div>
+    </>
+  );
+}
