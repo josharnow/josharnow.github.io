13706b542691e70453eb98576e97255313c19802
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-07-22 19:31:28 -0400

    Add comment

--- src/app/career-and-education-timeline/page.tsx
+++ src/app/career-and-education-timeline/page.tsx
@@ -6,6 +6,8 @@ export default function CareerAndEducationTimelinePage() {
 
   return (
     <>
+    {/* TODO - Outer component should be selector between education timeline & career timeline */}
+    {/* TODO - Inner component should be timeline for each type (education & career) */}
       <div className='h-full flex justify-center items-center'>
         <span className='font-medium text-3xl'>Coming soon!</span>
       </div>
