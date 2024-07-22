93edf72160edcdb6f587a0f6ad43831e5c5fb89c
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-07-21 23:35:34 -0400

    Moved Lid to "server component"

--- src/components/MacbookScroll/index.tsx
+++ src/components/MacbookScroll/index.tsx
@@ -1,6 +1,4 @@
 "use client";
-import { Slideshow } from "@/src/components";
-import Logo from "@/src/components/Navigation/Logo";
 import {
   IconBrightnessDown,
   IconBrightnessUp,
@@ -22,11 +20,15 @@ import {
   IconVolume3,
   IconWorld,
 } from "@tabler/icons-react";
-import { MotionValue, motion, useScroll, useTransform } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import React, { useEffect, useRef, useState } from "react";
+import Lid from "./Lid";
 
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+
+// export  Lid;
+export * from "./Lid";
 
 function useWindowSize() {
   // SOURCE - https://stackoverflow.com/questions/62846043/react-js-useeffect-with-window-resize-event-listener-not-working
@@ -214,61 +216,6 @@ const MacbookScroll = ({
       >
         { bottomContent }
       </motion.h2>
-    </div>
-  );
-};
-
-export const Lid = ({
-  scaleX,
-  scaleY,
-  rotate,
-  translate,
-  srcArray,
-}: {
-  scaleX: MotionValue<number>;
-  scaleY: MotionValue<number>;
-  rotate: MotionValue<number>;
-  translate: MotionValue<number>;
-  srcArray?: string[];
-}) => {
-  return (
-    <div className="relative [perspective:800px]">
-      <div
-        style={ {
-          transform: "perspective(800px) rotateX(-25deg) translateZ(0px)",
-          transformOrigin: "bottom",
-          transformStyle: "preserve-3d",
-        } }
-        className="h-[12rem] w-[32rem] bg-[#010101] rounded-2xl p-2 relative"
-      >
-        <div
-          style={ {
-            boxShadow: "0px 2px 0px 2px var(--neutral-900) inset",
-          } }
-          className="absolute inset-0 bg-[#010101] rounded-lg flex items-center justify-center"
-        >
-          <span className="text-white" style={{"width": 66, "height": 66}}>
-            <Logo />
-          </span>
-        </div>
-      </div>
-      <motion.div
-        style={ {
-          scaleX: scaleX,
-          scaleY: scaleY,
-          rotateX: rotate,
-          translateY: translate,
-          transformStyle: "preserve-3d",
-          transformOrigin: "top",
-        } }
-        className="h-96 w-[32rem] absolute inset-0 bg-[#010101] rounded-2xl p-2"
-      >
-        <div className="absolute inset-0 bg-[#272729] rounded-lg" />
-        <Slideshow
-          srcArray={ srcArray }
-          delay={ 4000 }
-        />
-      </motion.div>
     </div>
   );
 };
