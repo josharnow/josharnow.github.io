f5034360ae315f23cf9f5a59b439638db2abd500
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-07-28 16:25:10 -0400

    More progress with my custom Timeline component

--- src/components/Timeline/index.tsx
+++ src/components/Timeline/index.tsx
@@ -1,13 +1,20 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 type TimelineProps = {
   // className?: string;
   // fill?: string;
-  contentArr: object[];
+  contentArr: Content[];
 };
+
+type Content = {
+  yearStart: number;
+  yearEnd: number;
+  content: string;
+};
+
 
 // TODO - Make content into type
 
@@ -18,11 +25,22 @@ function cn(...inputs: ClassValue[]) {
 const Timeline = ({ 
   contentArr
 }: TimelineProps) => {
-  for (let i = 0; i < contentArr.length; i++) {
-    console.log(contentArr[i]);
-  }
-  // TODO - Extend timeline based on contentArr length
+  // NOTE - Sorts the content array by yearStart
+  const combinedYearsArr = [];
+  const sortedContentArr = contentArr.sort((a, b) => a.yearStart - b.yearStart);
 
+  for (let i = 0; i < sortedContentArr.length; i++) {
+    combinedYearsArr.push(sortedContentArr[i].yearStart);
+    combinedYearsArr.push(sortedContentArr[i].yearEnd);
+  }
+  const timelineYears = new Set(combinedYearsArr);
+
+  const [selectedYear, setSelectedYear] = useState(combinedYearsArr[0]);
+
+
+  function handleYearClick(e: React.MouseEvent<HTMLDivElement | HTMLSpanElement, MouseEvent>, year: number, yearIndex?: number) {
+    return setSelectedYear(year);
+  }
 
   return (
     <>
@@ -33,34 +51,23 @@ const Timeline = ({
       {/* TODO - Superimpose slider component over timeline so it can be scrolled by that...? */ }
       {/* NOTE - https://primereact.org/slider/ */ }
       <div className="flex gap-x-2 relative w-full justify-between">
-        <div className="flex flex-col items-center relative">
-          <div className="flex">
-            {/* NOTE - -left-1.5 is used to account for the radius of the circle so it can be properly centered */}
-            <div className="circle border w-3 h-3 rounded-full bg-blue-500 absolute -left-1.5 z-20"></div>
-          </div>
-          <span className="absolute top-3">2012</span>
-        </div>
-        
-        <div className="flex flex-col items-center relative">
-          <div className="flex">
-            <div className="circle border w-3 h-3 rounded-full bg-blue-500 absolute -left-1.5 z-20"></div>
-          </div>
-          <span className="absolute top-3">2014</span>
-        </div>
-        <div className="flex flex-col items-center relative">
-          <div className="flex">
-            <div className="circle border w-3 h-3 rounded-full bg-blue-500 absolute -left-1.5 z-20"></div>
-          </div>
-          <span className="absolute top-3">Present</span>
-        </div>
-        
-
-
-
-
-
-        
-        <div className="line bg-white h-[1px] w-full absolute top-1.5 z-10"></div>
+        { 
+          [...timelineYears].map((year, i) =>
+            // <ObjectRow key={ i } />
+            <>
+              <div key={i}>
+                <div className="flex flex-col items-center relative">
+                  <div className="flex">
+                    {/* NOTE - -left-1.5 is used to account for the radius of the circle so it can be properly centered */ }
+                    <div className="circle border w-3 h-3 rounded-full bg-blue-500 absolute -left-1.5 z-20 cursor-pointer" onClick={ (e) => handleYearClick(e, year, i) }></div>
+                  </div>
+                  <span className="absolute top-3 cursor-pointer" onClick={ (e) => handleYearClick(e, year, i) }>{ year }</span>
+                </div>
+              </div>
+            </>
+          ) 
+        }
+        <div className="line bg-white h-1 w-full absolute top-1 z-10"></div>
       </div>
     </>
   );
