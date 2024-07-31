1ee8b3e565449c97a6c888b9c473b5af4b2f2eaa
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-07-31 00:26:14 -0400

    Added WavyBackground to timeline component & more

--- src/components/CareerAndEducationTimelineContent/index.tsx
+++ src/components/CareerAndEducationTimelineContent/index.tsx
@@ -1,11 +1,12 @@
 "use client";
 import React, { useEffect, useRef, useState, useCallback } from "react";
+import { Timeline, WavyBackground } from "@/src/components";
 
 import { createNoise3D } from "simplex-noise";
 
-import { Stepper, StepperChangeEvent } from 'primereact/stepper';
-import { StepperPanel } from 'primereact/stepperpanel';
-import { Button } from 'primereact/button';
+// import { Stepper, StepperChangeEvent } from 'primereact/stepper';
+// import { StepperPanel } from 'primereact/stepperpanel';
+// import { Button } from 'primereact/button';
 
 
 import { type ClassValue, clsx } from "clsx";
@@ -18,54 +19,128 @@ function cn(...inputs: ClassValue[]) {
 const CareerAndEducationTimelineContent = () => {
   const stepperRef = useRef(null);
 
-  function handleStepperClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
-    if ((e.target as HTMLButtonElement).classList.contains('pi-arrow-left') || (e.target as HTMLSpanElement).textContent === 'Back') {
-      // @ts-ignore
-      stepperRef?.current?.prevCallback()
-    } else if ((e.target as HTMLButtonElement).classList.contains('pi-arrow-right') || (e.target as HTMLSpanElement).textContent === 'Next') {
-      // @ts-ignore
-      stepperRef?.current?.nextCallback() 
-    }
-  }
+  // function handleStepperClick(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+  //   if ((e.target as HTMLButtonElement).classList.contains('pi-arrow-left') || (e.target as HTMLSpanElement).textContent === 'Back') {
+  //     // @ts-ignore
+  //     stepperRef?.current?.prevCallback()
+  //   } else if ((e.target as HTMLButtonElement).classList.contains('pi-arrow-right') || (e.target as HTMLSpanElement).textContent === 'Next') {
+  //     // @ts-ignore
+  //     stepperRef?.current?.nextCallback() 
+  //   }
+  // }
     
+  const contentArr = [
+    {
+      yearStart: 2020,
+      yearEnd: 2021,
+      content: "Started working at company A"
+    },
+    {
+      yearStart: 2016,
+      yearEnd: 2019,
+      content: "Graduated from university B"
+    },
+    {
+      yearStart: 2018,
+      yearEnd: 2018,
+      content: "Started university B"
+    },
+    {
+      yearStart: 2017,
+      yearEnd: 2017,
+      content: "Graduated from high school"
+    },
+    {
+      yearStart: 2017,
+      yearEnd: 2017,
+      content: "Graduated from high school"
+    },
+  ]
+
+  const sortedContentArr = sortContentArr(contentArr);
+  const combinedYearsArr = combineYears(sortedContentArr);
+  const [selectedYear, setSelectedYear] = useState(combinedYearsArr[0]);
+
+  function sortContentArr(contentArr: TimelineContent[]) {
+    return contentArr.sort((a, b) => a.yearStart - b.yearStart);
+  }
+  function combineYears(contentArr: TimelineContent[]) {
+    const currentYear = new Date().getFullYear();
+    const combinedYearsArr = [];
+
+    for (let i = 0; i < contentArr.length; i++) {
+      combinedYearsArr.push(contentArr[i].yearStart);
+      combinedYearsArr.push(contentArr[i].yearEnd);
+    }
+
+    combinedYearsArr.push(currentYear);
+
+    const combinedYearsSet = new Set(combinedYearsArr);
+    return [...combinedYearsSet].sort((a, b) => a - b);
+  }
+
+  // function calculateTranslateX(year: number, timelineYears: number[]) {
+  function getYearRange(timelineYears: number[]) {
+    const yearRange = timelineYears[timelineYears.length - 1] - timelineYears[0];
+    return yearRange;
+  }
+
+  function performTimelineYearTranslate(i: number, timelineYears: number[]) {
+    const yearRange = getYearRange(timelineYears);
+    return `calc(${i} * ${100}%)`;
+
+    // TODO - figure out reference point... needd to be able to calculate the percentage of the timeline that each year represents
+    const minYear = Math.min(...timelineYears);
+    const maxYear = Math.max(...timelineYears);
+    const year = minYear + (yearRange * i);
+    console.log(year)
+    // console.log(minYear)
+    // console.log(maxYear)
+    // return `calc(${year} * ${100 / yearRange}%)`;
+    // return `calc(${i} * ${100 / yearRange}%)`;
+  }
+
+  function calculateYearFlexBasis(year: number, timelineYears: number[]) {
+    const yearRange = getYearRange(timelineYears);
+    return `calc(${year} * ${100 / yearRange}%)`;
+  }
+  function calculateFlexGrow(year: number, timelineYears: number[]) {
+    const yearRange = getYearRange(timelineYears);
+    return `calc(${year} * ${100 / yearRange}%)`;
+  }
 
   return (
     <>
-      <div className='h-full flex justify-center items-center'>
-        <Stepper ref={ stepperRef } pt={ {
-          root: {
-            style: {
-              flexBasis: '50rem',
-            },
-          }
-        } }>
-          {/* TODO - Make component for each stepper panel */ }
-          <StepperPanel header="Header I">
-            <div className="flex flex-col h-12rem">
-              <div className="border-2 border-dashed border-gray-300 rounded surface-ground flex-auto flex justify-center items-center font-medium">Content I</div>
+    {/* TODO - Draw how I want component to look */}
+    {/* TODO - Make my own component */}
+
+    {/* TODO - Make component for each stepper panel */ }
+
+
+    {/* <div className="w-full h-full px-6 py-10 sm:py-15 sm:px-24 md:px-20"> */}
+      <WavyBackground containerClassName="absolute top-0 left-0 right-0 bottom-0" className="w-full" />
+
+      <div className="absolute top-0 bottom-0 left-0 right-0 p-6">
+      {/* <div className="w-full h-full p-6"> */}
+        <div className="w-full h-full border">
+          {/* TODO - Career card above */}
+          {/* TODO - Timeline between */}
+          {/* TODO - Education card below */}
+
+          <div className="w-full h-full flex flex-col">
+              <div className="w-full grow border flex flex-col justify-center items-center">
+              <span>Career</span>
+              <b>{ selectedYear }</b>
             </div>
-            <div className="flex pt-4 justify-end">
-              <Button label="Next" icon="pi pi-arrow-right" iconPos="right" onClick={ handleStepperClick } />
+            <div className="w-full grow border flex flex-col justify-center items-center p-3">
+              <Timeline timelineYears={ combinedYearsArr } selectedYear={ selectedYear } setSelectedYear={ setSelectedYear } />
             </div>
-          </StepperPanel>
-          <StepperPanel header="Header II">
-            <div className="flex flex-col h-12rem">
-              <div className="border-2 border-dashed border-gray-300 rounded surface-ground flex-auto flex justify-center items-center font-medium">Content II</div>
+            <div className="w-full grow border flex flex-col justify-center items-center">
+              <span>Education</span>
+              <b>{selectedYear}</b>
             </div>
-            <div className="flex pt-4 justify-between">
-              <Button label="Back" severity="secondary" icon="pi pi-arrow-left" onClick={ handleStepperClick } />
-              <Button label="Next" icon="pi pi-arrow-right" iconPos="right" onClick={ handleStepperClick } />
-            </div>
-          </StepperPanel>
-          <StepperPanel header="Header III">
-            <div className="flex flex-col h-12rem">
-              <div className="border-2 border-dashed border-gray-300 rounded surface-ground flex-auto flex justify-center items-center font-medium">Content III</div>
-            </div>
-            <div className="flex pt-4 justify-start">
-              <Button label="Back" severity="secondary" icon="pi pi-arrow-left" onClick={ handleStepperClick } />
-            </div>
-          </StepperPanel>
-        </Stepper>
+          </div>
+        </div>
       </div>
     </>
   );
