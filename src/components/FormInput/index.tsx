36193b55fb9ca06a47ebdab689c7a38a2a6ec3b9
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-08-05 18:09:12 -0400

    Corrected dark/light mode remnants

--- src/components/FormInput/index.tsx
+++ src/components/FormInput/index.tsx
@@ -76,11 +76,11 @@ const FormInput = React.forwardRef<HTMLInputElement, InputProps>(
                 <input
                   type={ type }
                   className={ cn(
-                    `flex h-10 w-full border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
-          file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600 
-          focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600
+                    `flex h-10 w-full border-none bg-zinc-800 text-white rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent 
+          file:text-sm file:font-medium placeholder-text-neutral-600 
+          focus-visible:outline-none focus-visible:ring-[2px] focus-visible:ring-neutral-600
           disabled:cursor-not-allowed disabled:opacity-50
-          dark:shadow-[0px_0px_1px_1px_var(--neutral-700)]
+          shadow-[0px_0px_1px_1px_var(--neutral-700)]
           group-hover/input:shadow-none transition duration-400
           `,
                     className
@@ -99,7 +99,7 @@ const FormInput = React.forwardRef<HTMLInputElement, InputProps>(
                   role="textbox" 
                   contentEditable={ true } 
                   aria-placeholder="Your message"
-                  className={ cn(className, "border-none bg-gray-50 dark:bg-zinc-800 text-black dark:text-white shadow-input rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent  file:text-sm file:font-medium placeholder:text-neutral-400 dark:placeholder-text-neutral-600  focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-400 dark:focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 dark:shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400") }
+                  className={ cn(className, "border-none bg-zinc-800 text-white rounded-md px-3 py-2 text-sm  file:border-0 file:bg-transparent  file:text-sm file:font-medium placeholder-text-neutral-600  focus-visible:outline-none focus-visible:ring-[2px]  focus-visible:ring-neutral-600 disabled:cursor-not-allowed disabled:opacity-50 shadow-[0px_0px_1px_1px_var(--neutral-700)] group-hover/input:shadow-none transition duration-400") }
                 />
               </>
             )
