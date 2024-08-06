83577f75dc041588ac1793ba121c584351f0d8c8
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-08-05 20:45:01 -0400

    Improved card sizing

--- src/components/TimelineContent/CareerTimelineCard/index.tsx
+++ src/components/TimelineContent/CareerTimelineCard/index.tsx
@@ -23,21 +23,23 @@ const CareerTimelineCard = ({ content, className }: {
           > */}
         <CardSpotlight className={ cn(styles.card, "rounded-lg w-full h-full p-2 shadow-3xl shadow-blue-500 bg-black bg-opacity-80") }
           >
-            {/* TODO - Use CSS grid */}
+          {/* TODO - Use CSS grid */}
+          <div className="grid grid-cols-2">
+            <div className="flex h-fit">
+              <h1>{ content.yearStart }</h1>&nbsp;–&nbsp;<h1>{ content.yearEnd ? content.yearEnd : "Present" }</h1>
+            </div>
+            <h6 className="text-xs sm:text-sm h-fit" style={ { textAlign: "right" } }><i>{ content.location }</i></h6>
+          </div>
 
-          {/* TODO - Add  allow-discrete transition (need separate styles) */}
-          {/* <CardSpotlight className="border border-red-500 rounded-lg w-full h-full p-2 shadow-3xl shadow-white"> */}
-          <div className="flex justify-between w-full text-xs sm:text-sm">
+
+          {/* <div className="flex justify-between w-full text-xs sm:text-sm">
             <div className="flex">
               <h1>{ content.yearStart }</h1>&nbsp;–&nbsp;<h1>{ content.yearEnd ? content.yearEnd : "Present" }</h1>
             </div>
-            {/* <div className="flex justify-end"> */}
             <h6 className="text-xs sm:text-sm" style={ { textAlign: "right" } }><i>{ content.location }</i></h6>
-            {/* </div> */}
-          </div>
+          </div> */}
           <div className="flex">
             <h1 className="font-medium">{ content.institution }</h1>
-              {/* <h1 className="font-medium">{ content.location }</h1> */}
           </div>
         </CardSpotlight>
       </div>
