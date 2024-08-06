76c84ac907e8732a4d7031a058ab25b169056487
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-08-05 20:18:27 -0400

    More progress with card styling

--- src/components/TimelineContent/CareerTimelineCard/index.tsx
+++ src/components/TimelineContent/CareerTimelineCard/index.tsx
@@ -21,12 +21,24 @@ const CareerTimelineCard = ({ content, className }: {
         {/* <CardSpotlight className={ cn(styles.card, "border border-red-500 rounded-lg w-full h-full p-2 shadow-3xl shadow-blue-500 bg-transparent") }
           style={ { transition: "box-shadow 1s ease 0s, background 1s ease 0s, opacity 1s ease 0s, color 1s ease 1s allow-discrete" } }
           > */}
-        <CardSpotlight className={ cn(styles.card, "rounded-lg w-full h-full p-2 shadow-3xl shadow-blue-500 bg-transparent") }
+        <CardSpotlight className={ cn(styles.card, "rounded-lg w-full h-full p-2 shadow-3xl shadow-blue-500 bg-black bg-opacity-80") }
           >
+            {/* TODO - Use CSS grid */}
+
           {/* TODO - Add  allow-discrete transition (need separate styles) */}
-        {/* <CardSpotlight className="border border-red-500 rounded-lg w-full h-full p-2 shadow-3xl shadow-white"> */}
-          <h1>{ content.yearStart }</h1>
-          <h1>{ content.institution }</h1>
+          {/* <CardSpotlight className="border border-red-500 rounded-lg w-full h-full p-2 shadow-3xl shadow-white"> */}
+          <div className="flex justify-between w-full text-xs sm:text-sm">
+            <div className="flex">
+              <h1>{ content.yearStart }</h1>&nbsp;–&nbsp;<h1>{ content.yearEnd ? content.yearEnd : "Present" }</h1>
+            </div>
+            {/* <div className="flex justify-end"> */}
+            <h6 className="text-xs sm:text-sm" style={ { textAlign: "right" } }><i>{ content.location }</i></h6>
+            {/* </div> */}
+          </div>
+          <div className="flex">
+            <h1 className="font-medium">{ content.institution }</h1>
+              {/* <h1 className="font-medium">{ content.location }</h1> */}
+          </div>
         </CardSpotlight>
       </div>
     </>
