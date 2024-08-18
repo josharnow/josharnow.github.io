aac5e20a8a822f3e21e587064e91cc3cac80fd81
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-08-18 14:17:38 -0400

    Timeline formatting updates

--- src/components/TimelineContent/CareerTimelineCard/index.tsx
+++ src/components/TimelineContent/CareerTimelineCard/index.tsx
@@ -21,25 +21,25 @@ const CareerTimelineCard = ({ content, className }: {
         {/* <CardSpotlight className={ cn(styles.card, "border border-red-500 rounded-lg w-full h-full p-2 shadow-3xl shadow-blue-500 bg-transparent") }
           style={ { transition: "box-shadow 1s ease 0s, background 1s ease 0s, opacity 1s ease 0s, color 1s ease 1s allow-discrete" } }
           > */}
-        <CardSpotlight className={ cn(styles.card, "rounded-lg w-full h-full p-2 shadow-3xl shadow-blue-500 bg-black bg-opacity-80") }
+        <CardSpotlight className={ cn(styles.card, "flex flex-col gap-y-2 rounded-lg w-full h-full p-2 shadow-3xl shadow-blue-500 bg-black bg-opacity-80") }
           >
           {/* TODO - Use CSS grid */}
-          <div className="grid grid-cols-2">
-            <div className="flex h-fit">
-              <h1>{ content.yearStart }</h1>&nbsp;–&nbsp;<h1>{ content.yearEnd ? content.yearEnd : "Present" }</h1>
+          <div>
+
+            <div className="grid grid-cols-2">
+              <div className="text-xs sm:text-sm flex h-fit">
+                <h1>{ content.yearStart }</h1>&nbsp;–&nbsp;<h1>{ content.yearEnd ? content.yearEnd : "Present" }</h1>
+              </div>
+              <h6 className="text-xs sm:text-sm h-fit" style={ { textAlign: "right" } }><i>{ content.location }</i></h6>
             </div>
-            <h6 className="text-xs sm:text-sm h-fit" style={ { textAlign: "right" } }><i>{ content.location }</i></h6>
+            <div className="flex flex-col">
+              <h1 className="font-medium text-sm sm:text-base text-blue-500">{ content.institution }</h1>
+              <h3 className="font-medium text-xs sm:text-sm text-white"><i>{ content.position }</i></h3>
+            </div>
           </div>
 
-
-          {/* <div className="flex justify-between w-full text-xs sm:text-sm">
-            <div className="flex">
-              <h1>{ content.yearStart }</h1>&nbsp;–&nbsp;<h1>{ content.yearEnd ? content.yearEnd : "Present" }</h1>
-            </div>
-            <h6 className="text-xs sm:text-sm" style={ { textAlign: "right" } }><i>{ content.location }</i></h6>
-          </div> */}
-          <div className="flex">
-            <h1 className="font-medium">{ content.institution }</h1>
+          <div className="grow text-xs sm:text-sm">
+test
           </div>
         </CardSpotlight>
       </div>
