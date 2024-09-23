8d1dc028347b9bedf3ec5077788ea0db795d8dbc
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-09-23 15:47:45 -0400

    Removed CardSpotlight component wrapper to fix scrolling

--- src/components/TimelineContent/CareerTimelineCard/index.tsx
+++ src/components/TimelineContent/CareerTimelineCard/index.tsx
@@ -21,8 +21,8 @@ const CareerTimelineCard = ({ content, className }: {
         {/* <CardSpotlight className={ cn(styles.card, "border border-red-500 rounded-lg w-full h-full p-2 shadow-3xl shadow-blue-500 bg-transparent") }
           style={ { transition: "box-shadow 1s ease 0s, background 1s ease 0s, opacity 1s ease 0s, color 1s ease 1s allow-discrete" } }
           > */}
-        <CardSpotlight className={ cn(styles.card, "flex flex-col gap-y-2 rounded-lg w-full h-full p-2 shadow-3xl shadow-blue-500 bg-black bg-opacity-80") }
-          >
+        {/* <CardSpotlight className={ cn(styles.card, "flex flex-col gap-y-2 rounded-lg w-full h-full p-2 shadow-3xl shadow-blue-500 bg-black bg-opacity-80") }> */}
+        <div className={ cn(styles.card, "flex flex-col gap-y-2 rounded-lg w-full h-full p-2 shadow-3xl shadow-blue-500 bg-black bg-opacity-80") }>
           <div>
             <div className="grid grid-cols-2">
               <div className="text-xs sm:text-sm md:text-base xl:text-lg flex h-fit">
@@ -42,7 +42,7 @@ const CareerTimelineCard = ({ content, className }: {
           <div className="grow text-sm sm:text-base md:text-lg xl:text-xl overflow-y-scroll flex flex-col" style={ { "flexBasis": 0, "justifyContent": "safe center" } }>
             { content.bodyElement }
           </div>
-        </CardSpotlight>
+        </div>
       </div>
     </>
   );
