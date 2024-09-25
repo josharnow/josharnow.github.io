8ee0092b957b91f48726baef1107be8bc8f8d38e
Josh Arnow <1476967+josharnow@users.noreply.github.com>
2024-09-25 17:46:58 -0400

    Added hovering transitions when hovering over next buttons

--- src/components/AnimatedTabs/index.tsx
+++ src/components/AnimatedTabs/index.tsx
@@ -117,13 +117,17 @@ const Tabs = ({
         <button 
           className="inline-flex size-12 animate-shimmer items-center justify-center rounded-2xl border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] p-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
           onClick={ (e) => handleBackClick(e, tabs, active) }
-          >
+          onMouseEnter={ () => setHovering(true) }
+          onMouseLeave={ () => setHovering(false) }
+        >
           <i className="pi pi-chevron-left"></i>
         </button>
         <button
           className="inline-flex size-12 animate-shimmer items-center justify-center rounded-2xl border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] p-6 font-medium text-white transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50"
           onClick={ (e) => handleNextClick(e, tabs, active) }
-          >
+          onMouseEnter={ () => setHovering(true) }
+          onMouseLeave={ () => setHovering(false) }
+        >
           <i className="pi pi-chevron-right"></i>
         </button>
       </div>
